c4783348b2d02a8e897bc2df14a7734ee8cef0f2
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2024-03-17 05:55:53 -0400

    Refactor code displaying modal to eliminate dual maintenance.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,20 +6,11 @@ import React from 'react'
 export default function Modal({ isCorrect, solution, turn }) {
   return (
     <div className="modal">
-      {isCorrect && (
-        <div>
-          <h1>You Win!</h1>
-          <p className="solution">{solution}</p>
-          <p>You found the solution in {turn} guesses :)</p>
-        </div>
-      )}
-      {!isCorrect && (
-        <div>
-          <h1>Nevermind</h1>
-          <p className="solution">{solution}</p>
-          <p>Better luck next time :)</p>
-        </div>
-      )}
+      <div>
+        <h1>{isCorrect ? "You win!" : "Nevermind"}</h1>
+        <p className="solution">{solution}</p>
+        <p>{isCorrect ? `You found the solution in ${turn} guesses :)` : "Better luck next time :)"}</p>
+      </div>
     </div>
   )
 }
